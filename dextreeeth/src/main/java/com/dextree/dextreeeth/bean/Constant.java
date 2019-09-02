a4225086e097899e94cde4a8aaa95f81b0253a6a
yangwenlong <wenlong@dextree.com>
2019-09-02 13:19:33 +0800

    上传

--- dextreeeth/src/main/java/com/dextree/dextreeeth/bean/Constant.java
+++ dextreeeth/src/main/java/com/dextree/dextreeeth/bean/Constant.java
@@ -8,12 +8,12 @@ package com.dextree.dextreeeth.bean;
 public class Constant {
 
 
-////    //主网
-//    public static final String URLTRANSACTION = "https://api.etherscan.io/";
-//    public static final String MAInURL = "https://mainnet.infura.io/v3/c86557f922384086ac5f500137f8c88a";
-//    public static final String MAINADDRESS = "0xd27d76a1ba55ce5c0291ccd04febbe793d22ebf4";
+//    //主网
+    public static final String URLTRANSACTION = "https://api.etherscan.io/";
+    public static final String MAInURL = "https://mainnet.infura.io/v3/c86557f922384086ac5f500137f8c88a";
+    public static final String MAINADDRESS = "0xd27d76a1ba55ce5c0291ccd04febbe793d22ebf4";
 //    测试网
-     public static final String MAInURL = "https://rinkeby.infura.io/v3/c86557f922384086ac5f500137f8c88a";
-     public static final String MAINADDRESS = "0x918b11852933f8a72EDABaB30F24fe0268F6b4CE";
-     public static final String URLTRANSACTION= "https://api-rinkeby.etherscan.io/";
+//     public static final String MAInURL = "https://rinkeby.infura.io/v3/c86557f922384086ac5f500137f8c88a";
+//     public static final String MAINADDRESS = "0x918b11852933f8a72EDABaB30F24fe0268F6b4CE";
+//     public static final String URLTRANSACTION= "https://api-rinkeby.etherscan.io/";
 }
